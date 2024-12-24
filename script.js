481f011bfab69594f0a6d4a969a2ebe2bb6673c5
Bojan Ludajic <bojan.ludajic@gmail.com>
2024-12-24 17:44:47 +0100

    updated projects and fixed dark mode not persisting

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
 const body = document.body;
+const icon = toggleDarkModeBtn.querySelector('i');
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+    body.classList.add('dark-mode');
+    icon.classList.replace('bx-moon', 'bx-sun');
+} else {
+    body.classList.remove('dark-mode');
+    icon.classList.replace('bx-sun', 'bx-moon');
+}
 
 toggleDarkModeBtn.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
-    const icon = toggleDarkModeBtn.querySelector('i');
     if (body.classList.contains('dark-mode')) {
         icon.classList.replace('bx-moon', 'bx-sun');
+        localStorage.setItem('darkMode', 'enabled');
     } else {
         icon.classList.replace('bx-sun', 'bx-moon');
+        localStorage.setItem('darkMode', 'disabled');
     }
 });
 
